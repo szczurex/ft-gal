c90fba58ecfa664ac4c99dfa08c00b1b12fc996b
Łukasz Zarzecki <lukasz.zarzecki@fwioo.pl>
2013-09-09 16:36:13 +0200

    mainpage alpha

--- furtown/urls.py
+++ furtown/urls.py
@@ -15,4 +15,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^profiles/', include('profiles.urls', namespace="profiles")),
+    url(r'^', include('main.urls', namespace="main")),
 )
--- main/views.py
+++ main/views.py
@@ -1,1 +1,6 @@
 # Create your views here.
+from django.shortcuts import render
+
+
+def index(request, template="main/index.html"):
+    return render(request, template, {})
