5b06fdfc3f64830dce5d82c7aab2090baf3ab60b
Łukasz Zarzecki <lukasz.zarzecki@fwioo.pl>
2013-09-17 13:47:38 +0200

    submission model, file validation

--- gallery/models.py
+++ gallery/models.py
@@ -2,6 +2,54 @@ from django.db import models
 from profiles.models import Profile
 
 # TODO: file verification
+from gallery.validators import FileValidator
+
+# Thumbs = 512 kB
+# Files = 2048 kB
+
+# audio, images, flash - docs/rtf/odt maybe soonish.
+FILE_ALLOWED_MIMES = (
+    'audio/mpeg',
+    'image/gif',
+    'image/jpeg',
+    'image/pjpeg',
+    'image/png',
+    'image/bmp',
+    'image/x-bmp',
+    'application/x-shockwave-flash',
+)
+FILE_ALLOWED_EXTENSIONS = (
+    'mp3',
+    'gif',
+    'jpg',
+    'png',
+    'bmp',
+    'swf',
+)
+THUMB_ALLOWED_MIMES = (
+    'image/gif',
+    'image/jpeg',
+    'image/pjpeg',
+    'image/png',
+    'image/bmp',
+    'image/x-bmp',
+)
+THUMB_ALLOWED_EXTENSIONS = (
+    'gif',
+    'jpg',
+    'png',
+    'bmp',
+)
+
+# TODO: pull sizes from settings
+
+THUMB_VALIDATOR = FileValidator(max_size=512*1024,
+                                allowed_extensions=THUMB_ALLOWED_EXTENSIONS,
+                                allowd_mimetypes=THUMB_ALLOWED_MIMES )
+FILE_VALIDATOR = FileValidator(max_size=2*1024*1024,
+                               allowed_extensions=FILE_ALLOWED_EXTENSIONS,
+                               allowd_mimetypes=FILE_ALLOWED_MIMES )
+
 
 RATING_GENERAL  = 1
 RATING_MATURE   = 2
@@ -18,6 +66,14 @@ FILE_TYPE_WRITING   = 2
 FILE_TYPE_MUSIC     = 3
 FILE_TYPE_FLASH     = 4
 
+FILE_TYPES = (
+    (FILE_TYPE_IMAGE, 'Image'),
+    (FILE_TYPE_WRITING, 'Text'),
+    (FILE_TYPE_MUSIC, 'Music'),
+    (FILE_TYPE_FLASH, 'Flash'),
+)
+
+
 class Submission(models.Model):
     profile = models.ForeignKey(Profile)
     # meta
@@ -27,9 +83,11 @@ class Submission(models.Model):
     # file
     file = models.FileField(verbose_name="File uploaded", max_length=200, upload_to="submissions")
     thumb = models.ImageField(verbose_name="Custom thumbnail", max_length=200, upload_to="submissions_thumbs")
-    #file_type = models.CharField(verbose_name="Submission type", choices=)
+    file_type = models.SmallIntegerField(verbose_name="Submission type", choices=FILE_TYPES)
     # internals
     date_created = models.DateTimeField(verbose_name="Date created", auto_now_add=True)
     date_edited = models.DateTimeField(verbose_name="Date modified", auto_now=True)
     hidden = models.BooleanField(verbose_name='Hidden', default=False)
-    deleted = models.BooleanField(verbose_name='Journal deleted?', default=False)
+    deleted = models.BooleanField(verbose_name='Submission deleted?', default=False)
+    
+    
