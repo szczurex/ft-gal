c9a91fd8c93f82e34dfbe73aae5db623a2a0378d
vilczus@gmail.com <lukasz.zarzecki@fwioo.pl>
2013-09-20 15:02:37 +0200

    notification init

--- messages/models.py
+++ messages/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.generic import GenericForeignKey
+from profiles.models import Profile
 
 class TaskedMail(models.Model):
     subject         = models.CharField(verbose_name='Mail subject', max_length=150)
@@ -8,5 +10,18 @@ class TaskedMail(models.Model):
     success         = models.BooleanField(verbose_name='Mail sent status', default=False)
     date_created    = models.DateTimeField(verbose_name='Mail creation date', auto_now_add=True)
     
+
+class Notification(models.Model):
+    profile = models.ForeignKey(Profile, related_name="notif_profile")
+    author = models.ForeignKey(Profile, related_name="notif_author")
     
-#class Notification
+    #submission/journal/comment/favourite/watch GenericKey
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    
+    
+    date_created = models.DateTimeField(verbose_name='Creation date', auto_now_add=True)
+    
+    class Meta:
+        unique_together = ("profile", "author")
